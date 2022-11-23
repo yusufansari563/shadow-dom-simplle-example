1a8b366f663a95d8523eb639ff5a7c06185c8d91
yusufansari563 <yusufansari563@gmail.com>
2022-11-24 00:53:53 +0530

    attached some extra with event dispatch

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-
-class MyProduct extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode:"open"});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.querySelector('section').innerHTML = this.htmlcomponent;
-        console.log(this.shadowRoot);
-    }
-    htmlcomponent = `<h1>html customElements</h1>`
+class MyProduct extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("section").innerHTML = this.htmlcomponent;
+    // this.shadowRoot.styleSheets[0].href = "";
+    console.log(this.shadowRoot);
+  }
+  htmlcomponent = `
+   <h1>html customElements</h1>
+   <button onclick="child()"> button</button>
+   `;
+}
+function child() {
+  demo();
 }
 
-const template = document.createElement('template');
-template.innerHTML = `<style>h1{color:red}</style><section></section>`;
+const template = document.createElement("template");
+template.innerHTML = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><section class="container"></section>`;
 
 window.customElements.define("my-product", MyProduct);
-
